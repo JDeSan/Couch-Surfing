d140e3506f3f4a2052763d9b23c9ea22b0b42bdd
John Lott <lott.john99@gmail.com>
2023-11-30 19:41:20 -0500

    added another js to run code

---
+++ Assets/runscript.js
@@ -0,0 +1,19 @@
+var searchMovieEl = document.querySelector('#search-movie');
+
+function handleSearchMovieSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
